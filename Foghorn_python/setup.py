99d7f0ea5844cea6fa977f2c63c2f57e79445dec
Declan Bennett <declanbennett@hotmail.com>
2018-05-17 10:40:31 +0100

    removed setup.py

--- Foghorn_python/setup.py
+++
@@ -1,8 +0,0 @@
-setup(name='Foghorn_python',
-      version='1.0',
-      description='Python implementation of Foghorn VCF transformation',
-      author='Declan Bennett',
-      author_email='declanbennett@hotmail.com',
-      url='',
-      packages=['Foghorn_python']
-     )
