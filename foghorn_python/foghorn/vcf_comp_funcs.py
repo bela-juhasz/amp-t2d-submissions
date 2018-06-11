97423000cc09959e66488039078de4a96bc5a8e6
Zhicheng-Liu <zl@ebi.ac.uk>
2018-06-11 16:53:22 +0100

    Remove redudant code statements

--- foghorn_python/foghorn/vcf_comp_funcs.py
+++ foghorn_python/foghorn/vcf_comp_funcs.py
@@ -3,58 +3,34 @@ import sys
 def GT_comp(GT, lines_holder, sample_index, mis, fields):
     if GT in {"0|1", "1|0", "1/0", "0/1"}:
         lines_holder.append("".join([str(sample_index), ":", "1"]))
-        sample_index = sample_index + 1
-        return sample_index, lines_holder
     elif GT in {"1|1", "1/1"}:
         lines_holder.append("".join([str(sample_index), ":", "2"]))
-        sample_index = sample_index + 1
-        return sample_index, lines_holder
     elif GT in {".|1", "1|.", "./1", "1/.", ".|0", "0|.", "./0", "0/."}:  # missing variants, Key value with variants and sample is better than this. test dataset has little missing variants so not an issue just yet
         string = "".join(["Sample# ", str(sample_index), " has a single allele missing at pos ->  ", str(fields[:4]), "\n"])
         mis.append(string)
-        sample_index = sample_index + 1
-        return sample_index, mis
     elif GT in {"./.", ".|."}:
         string = "".join(["Sample# ", str(sample_index), " has a both alleles missing at pos ->  ", str(fields[:4]), "\n"])
         mis.append(string)
-        sample_index = sample_index + 1
-        return sample_index, mis
-    else:
-        sample_index = sample_index + 1
-        return sample_index
 
 def DS_comp(DS, lines_holder, sample_index):
     if float(DS) != 0:
         string = "".join([str(sample_index), ":", str(DS)])
         lines_holder.append(string)
-        sample_index = sample_index + 1
-        return lines_holder, sample_index
-    else:
-        sample_index = sample_index + 1
-        return sample_index
 
 def mult_al(GT, lines_holderMS, sample_indexMS, mis, fields):
     if GT in {"2|2", "2/2"}:
         string = "".join([str(sample_indexMS), ":", "2"])
         lines_holderMS.append(string)
-        return sample_indexMS, lines_holderMS
     elif GT in {"0|2", "2|0", "2/0", "0/2"}:  # need to clarify the odds of seeing 1|2. This will most probably be a sequencing error. So for the moment I am only including the referernce "0"
         # This is one implementation the second is to just add extra indices
         string = "".join([str(sample_indexMS), ":", "1"])
         lines_holderMS.append(string)
-        return sample_indexMS, lines_holderMS
     elif GT in {".|.", "./."}:
         string = "".join(["Sample# ", str(sample_indexMS), " has a both alleles missing at pos ->  ", str(fields[:5]), "\n"])
         mis.append(string)
-        sample_indexMS = sample_indexMS + 1
-        return sample_indexMS, mis
     elif GT in {".|2", "2|.", "2/.", "./2", ".|1", "1|.", "1/.", "./1", ".|0", "0|.", "0/.", "./0"}:
         string = "".join(["Sample# ", str(sample_indexMS), " has asingle allele missing at pos ->  ", str(fields[:5]), "\n"])
         mis.append(string)
-        sample_indexMS = sample_indexMS + 1
-        return sample_indexMS, mis
-    else:
-        return None
 
 def GT(line, out, mis_vars):
     if not line.startswith("##") and not line.startswith("#"):  # Need to spend time making robust and reporting file format and missingness errors
@@ -68,8 +44,8 @@ def GT(line, out, mis_vars):
         lines_holder.extend(fields[:4])  # append first few columns
         if "," in fields[4]:
             lines_holderMS.extend(fields[:4])
-            lines_holder.append(str(str(fields[4]).split(",")[0]))  # ALT1
-            lines_holderMS.append(str(str(fields[4]).split(",")[1]))  # ALT2
+            lines_holder.append(str(fields[4]).split(",")[0])  # ALT1
+            lines_holderMS.append(str(fields[4]).split(",")[1])  # ALT2
             for column in fields[9:]:
                 GT = column.strip().split(":")[0]
                 GT_comp(GT, lines_holder, sample_index, mis, fields)
@@ -108,7 +84,7 @@ def DS(line, out):
         lines_holder = []
         lines_holder.extend(fields[:4]) # append first few columns
         if "," in fields[4]:  # not set up for multiple variants yet this is redundant until multivariants are incorported
-            lines_holder.append(str(str(fields[4]).split(",")[0]))  # ALT1
+            lines_holder.append(str(fields[4]).split(",")[0])  # ALT1
             for column in fields[9:]:
                 DS = column.strip().split(":")[int(DOSidx)]
                 DS_comp(DS, lines_holder, sample_index)
