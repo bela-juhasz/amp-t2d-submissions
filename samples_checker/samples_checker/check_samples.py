175ca45ad797f0247f2dc3f31374e0810ca2dea0
sundarvenkata-EBI <sundarvenkata@ebi.ac.uk>
2018-07-30 17:09:56 +0100

    Revert "EVA-1027 - Compare sample names in VCF and Submission Metadata sheet"

--- samples_checker/samples_checker/check_samples.py
+++ samples_checker/samples_checker/check_samples.py
@@ -7,57 +7,43 @@ import sys
 import argparse
 import utils
 
+arg_parser = argparse.ArgumentParser(description='Check submission xls contains all samples that'
+                                                 'exist in the submitted files')
+arg_parser.add_argument('--sample-xml', required=True, dest='samplexml',
+                        help='XML file containing submission samples')
+arg_parser.add_argument('--file-xml', required=True, dest='filexml',
+                        help='XML file containing submitted files')
+arg_parser.add_argument('--file-path', required=True, dest='filepath',
+                        help='Path to the directory in which submitted files can be found')
 
-def get_sample_diff(file_path, file_xml, sample_xml, submission_type="t2d"):
-    submission_samples = utils.get_samples_from_xml(sample_xml)
-    submitted_files = utils.get_files_from_xml(file_xml)
-    has_difference = False
-    for file_name in submitted_files:
-        file_type = submitted_files.get(file_name, '')
-        difference_submission_xls_submitted_file, difference_submitted_file_submission_xls = \
-            utils.get_sample_difference(submission_samples, file_path + '/' + file_name, file_type)
+args = arg_parser.parse_args()
+sample_xml = args.samplexml
+file_xml = args.filexml
+file_path = args.filepath
 
-        if difference_submitted_file_submission_xls:
-            has_difference = True
-            if file_type == 'vcf':
-                if submission_type == "t2d":
-                    print('The submission does not contain the following genotype ids:', file=sys.stderr)
-                if submission_type == "eva":
-                    print('Samples that appear in the VCF but not in the Metadata sheet:', file=sys.stderr)
-            else:
-                print('The submission does not contain the following samples:', file=sys.stderr)
-            print(difference_submitted_file_submission_xls, file=sys.stderr)
+submission_samples = utils.get_samples_from_xml(sample_xml)
+submitted_files = utils.get_files_from_xml(file_xml)
 
-        if difference_submission_xls_submitted_file:
-            has_difference = True
-            if submission_type == "t2d":
-                print('The following samples are not found in file ' + file_name, file=sys.stderr)
-            if submission_type == "eva":
-                print('Samples that appear in the Metadata sheet but not in the VCF: ' + file_name,
-                      file=sys.stderr)
-            print(difference_submission_xls_submitted_file, file=sys.stderr)
-    if has_difference:
-        quit(1)
-    print('Samples checking completed!', file=sys.stdout)
+has_difference = False
+for file_name in submitted_files:
+    file_type = submitted_files.get(file_name, '')
+    difference_submission_xls_submitted_file, difference_submitted_file_submission_xls =\
+        utils.get_sample_difference(submission_samples, file_path + '/' + file_name, file_type)
 
+    if difference_submitted_file_submission_xls:
+        has_difference = True
+        if file_type == 'vcf':
+            print('The submission does not contain the following genotype ids:', file=sys.stderr)
+        else:
+            print('The submission does not contain the following samples:', file=sys.stderr)
+        print(difference_submitted_file_submission_xls, file=sys.stderr)
 
-def main():
-    arg_parser = argparse.ArgumentParser(description='Check submission xls contains all samples that'
-                                                     'exist in the submitted files')
-    arg_parser.add_argument('--sample-xml', required=True, dest='samplexml',
-                            help='XML file containing submission samples')
-    arg_parser.add_argument('--file-xml', required=True, dest='filexml',
-                            help='XML file containing submitted files')
-    arg_parser.add_argument('--file-path', required=True, dest='filepath',
-                            help='Path to the directory in which submitted files can be found')
+    if difference_submission_xls_submitted_file:
+        has_difference = True
+        print('The following samples are not found in file '+file_name, file=sys.stderr)
+        print(difference_submission_xls_submitted_file, file=sys.stderr)
 
-    args = arg_parser.parse_args()
-    sample_xml = args.samplexml
-    file_xml = args.filexml
-    file_path = args.filepath
+if has_difference:
+    quit(1)
 
-    get_sample_diff(file_path, file_xml, sample_xml)
-
-
-if __name__ == "__main__":
-    main()
+print('Samples checking completed!', file=sys.stdout)
--- xls2xml/xls2xml/xls2xml.py
+++ xls2xml/xls2xml/xls2xml.py
@@ -9,44 +9,37 @@ import argparse
 from XLSReader import XLSReader
 import utils
 
+arg_parser = argparse.ArgumentParser(
+    description='Transform and output validated data from an excel file to a XML file')
+arg_parser.add_argument('xls', help='Excel file to be validated and transformed')
+arg_parser.add_argument('xml', help='XML file to be written to')
+arg_parser.add_argument('--conf', required=True, dest='conf',
+                        help='Configuration file contains list of worksheets and fields')
+arg_parser.add_argument('--conf-key', required=True, dest='confKey',
+                        help='Keys/tabs (comma delimited) to retrieve the list of field')
+arg_parser.add_argument('--schema', required=True, dest='schema',
+                        help='Schema definition for data field')
+arg_parser.add_argument('--xslt', required=True, dest='xslt',
+                        help='Definition for transformation from xls to xml document')
 
-def convert_xls_to_xml(xls_conf, xls_conf_keys, xls_schema, xslt_filename, xls_filename, xml_filename):
-    xls_reader = XLSReader(xls_filename, xls_conf)
-    xls_readers = [(key, xls_reader) for key in xls_conf_keys]
-    try:
-        output_xml = utils.multiple_objects_to_xml(xls_readers, xls_schema, xslt_filename)
-    except Exception as e:
-        print(e.message, file=sys.stderr)
-        quit(1)
-    with open(xml_filename, 'w') as xml_file:
-        utils.save_xml(output_xml, xml_file)
-    print('Conversion complete!', file=sys.stdout)
+args = arg_parser.parse_args()
+xls_filename = args.xls
+xml_filename = args.xml
+xls_conf = args.conf
+xls_conf_keys = args.confKey.split(',')
+xls_schema = args.schema
+xslt_filename = args.xslt
 
+xls_reader = XLSReader(xls_filename, xls_conf)
+xls_readers = [ (key, xls_reader) for key in xls_conf_keys ]
 
-def main():
-    arg_parser = argparse.ArgumentParser(
-        description='Transform and output validated data from an excel file to a XML file')
-    arg_parser.add_argument('xls', help='Excel file to be validated and transformed')
-    arg_parser.add_argument('xml', help='XML file to be written to')
-    arg_parser.add_argument('--conf', required=True, dest='conf',
-                            help='Configuration file contains list of worksheets and fields')
-    arg_parser.add_argument('--conf-key', required=True, dest='confKey',
-                            help='Keys/tabs (comma delimited) to retrieve the list of field')
-    arg_parser.add_argument('--schema', required=True, dest='schema',
-                            help='Schema definition for data field')
-    arg_parser.add_argument('--xslt', required=True, dest='xslt',
-                            help='Definition for transformation from xls to xml document')
+try:
+    output_xml = utils.multiple_objects_to_xml(xls_readers, xls_schema, xslt_filename)
+except Exception as e:
+    print(e.message, file=sys.stderr)
+    quit(1)
 
-    args = arg_parser.parse_args()
-    xls_filename = args.xls
-    xml_filename = args.xml
-    xls_conf = args.conf
-    xls_conf_keys = args.confKey.split(',')
-    xls_schema = args.schema
-    xslt_filename = args.xslt
+with open(xml_filename, 'w') as xml_file:
+    utils.save_xml(output_xml, xml_file)
 
-    convert_xls_to_xml(xls_conf, xls_conf_keys, xls_schema, xslt_filename, xls_filename, xml_filename)
-
-
-if __name__ == "__main__":
-    main()
+print('Conversion complete!', file=sys.stdout)
