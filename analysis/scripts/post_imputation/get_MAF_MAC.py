f51683eb7cb84a844a899547627efb4b3f5f5ab1
selva <selva@ebi.ac.uk>
2019-05-09 10:47:14 +0100

    modified MAC_MAF latest

--- analysis/scripts/post_imputation/get_MAF_MAC.py
+++ analysis/scripts/post_imputation/get_MAF_MAC.py
@@ -3,26 +3,22 @@ import sys
 param = sys.argv
 
 
-with open("MAC_MAF.txt","w") as OUT:
+with open("MAF_MAC_from_query.txt","w") as OUT:
 	string="VAR_ID" + "\t" + "MAF" + "\t" + "AN" + "\t"+ "MAC" + "\n"
 	OUT.write(string)
 	for line in sys.stdin:
-		if not line.startswith("#"):
-			fields = line.strip().split()
-			var_id = fields[0] + "_" + fields[1] + "_" + fields[3] + "_" + fields[4]
-			INFO= fields[5].strip().split(";")
-			for i in range(len(INFO)):
-				if INFO[i].split("=")[0] =="AC":
-					AC = INFO[i].split("=")[1]
-				elif INFO[i].split("=")[0] =="AN":
-					AN = INFO[i].split("=")[1]
-				elif INFO[i].split("=")[0] =="MAF":
-					MAF = INFO[i].split("=")[1]
-			if int(AC) < (float(AN)/2):
-				out = var_id + "\t" + MAF + "\t" + AN + "\t"+ AC + "\n"
-				OUT.write(out)
-			elif int(AC) > (float(AN)/2):
-				MAC = int(AN) - int(AC)
-				out = var_id + "\t" + MAF + "\t" + AN + "\t"
-				outl = out + str(MAC) + "\n"
-				OUT.write(outl)
+		line = line.strip()
+		parts = line.split("\t")
+		fields = parts
+		var_id = fields[0] + "_" + fields[1] + "_" + fields[2] + "_" + fields[3]
+		AF = fields[5]
+		MAF = fields[4]
+		AN = fields[6]
+		AC = fields[7]
+		if float(AC) <= (float(AN)/2):
+			out = var_id + "\t" + MAF + "\t" + AN + "\t"+ AC + "\n"
+			OUT.write(out)
+		elif float(AC) > (float(AN)/2):
+			MAC = int(AN) - int(AC)
+			out = var_id + "\t" + MAF + "\t" + AN + "\t" + str(MAC) +"\n"
+			OUT.write(out)
